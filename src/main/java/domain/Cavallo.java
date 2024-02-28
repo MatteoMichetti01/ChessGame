3ee8e33d3bc10e8bc12c9e95f0b660ecd140b970
Antonio-Addario <104568404+Antonio-Addario@users.noreply.github.com>
2024-02-28 17:58:06 +0100

    aggiunta classi pezzi

---
+++ src/main/java/domain/Cavallo.java
@@ -0,0 +1,8 @@
+package domain;
+
+public class Cavallo  extends Pezzo{
+
+    public Cavallo(String n, String c, int pX, int pY) {
+        super(n, c, pX, pY);
+    }
+}
---
+++ src/main/java/domain/Pedone.java
@@ -0,0 +1,8 @@
+package domain;
+
+public class Pedone extends Pezzo {
+
+    public Pedone(String n, String c, int pX, int pY) {
+        super(n, c, pX, pY);
+    }
+}
