8e0939ef29ced97e6f479a84c7e79e693e64eefd
Alessandro Consalvi <alessandroconsalvi.01@gmail.com>
2024-02-28 18:05:04 +0100

    aggiunta classi alfiere e re

---
+++ src/main/java/domain/Alfiere.java
@@ -0,0 +1,7 @@
+package domain;
+
+public class Alfiere extends Pezzo{
+    public Alfiere(String n, String c, int pX, int pY) {
+        super(n, c, pX, pY);
+    }
+}
---
+++ src/main/java/domain/Re.java
@@ -0,0 +1,7 @@
+package domain;
+
+public class Re extends Pezzo {
+    public Re(String n, String c, int pX, int pY) {
+        super(n, c, pX, pY);
+    }
+}
