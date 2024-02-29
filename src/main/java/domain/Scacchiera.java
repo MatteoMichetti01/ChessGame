043384187e263290daa1aa5ee2dcaff7f21ccdbb
Antonio-Addario <104568404+Antonio-Addario@users.noreply.github.com>
2024-02-29 15:19:46 +0100

    miglioramento scacchiera

--- src/main/java/domain/Scacchiera.java
+++ src/main/java/domain/Scacchiera.java
@@ -11,39 +11,66 @@ public class Scacchiera {
 
 
     }
-    public Pezzo[][] creazioneScacchiera(Pezzo[][] c){
+    public Pezzo[][] creazioneScacchiera(Pezzo[][] c) {
+        c[0][0] = new CasellaVuota("   ", "   ");
+        c[0][1] = new CasellaVuota(" A   ", "   ");
+        c[0][2] = new CasellaVuota("  B   ", "   ");
+        c[0][3] = new CasellaVuota("  C   ", "   ");
+        c[0][4] = new CasellaVuota("  D   ", "   ");
+        c[0][5] = new CasellaVuota("  E   ", "   ");
+        c[0][6] = new CasellaVuota("  F   ", "   ");
+        c[0][7] = new CasellaVuota("  G   ", "   ");
+        c[0][8] = new CasellaVuota("  H", "   ");
 
-                c[0][0] = new Torre("t1","nero");
-                c[0][1] = new Cavallo("c1","nero");
-                c[0][2] = new Alfiere("a1","nero");
-                c[0][3] =new Regina("queen","nero");
-                c[0][4] = new Re("re","nero");
-                c[0][5] = new Alfiere("a2","nero");
-                c[0][6] = new Cavallo("c2","nero");
-                c[0][7] = new Torre("t2","nero");
-                for (int i = 0; i < 8; i++) {
-                        c[1][i] = new Pedone("p"+i,"nero");
-                    }
-                for (int i = 0; i < 8; i++) {
-                    for (int j = 0; j < 8; j++) {
-                        if (c[i][j] == null) c[i][j] = new CasellaVuota(" ", null);
-                    }
-                }
-                c[7][0] = new Torre("t1","bianco");
-                c[7][1] = new Cavallo("c1","bianco");
-                c[7][2] = new Alfiere("a1","bianco");
-                c[7][3] =new Regina("queen","bianco");
-                c[7][4] = new Re("re","bianco");
-                c[7][5] = new Alfiere("a2","bianco");
-                c[7][6] = new Cavallo("c2","bianco");
-                c[7][7] = new Torre("t2","bianco");
-                for (int i = 0; i < 8; i++) {
-                    c[6][i] = new Pedone("p"+i,"bianco");
-                }
+        for (int i = 1; i <= 8; i++) {
+            c[i][0] = new CasellaVuota("" + (9 - i) + "", "   ");
+            for (int j = 1; j <= 8; j++) {
+                c[i][j] = new CasellaVuota("|    |", "   ");
+            }
+        }
 
+        c[1][1] = new Torre("| t1 |", "nero");
+        c[1][2] = new Cavallo("| c1 |", "nero");
+        c[1][3] = new Alfiere("| a1 |", "nero");
+        c[1][4] = new Regina("| q  |", "nero");
+        c[1][5] = new Re("| re |", "nero");
+        c[1][6] = new Alfiere("| a2 |", "nero");
+        c[1][7] = new Cavallo("| c2 |", "nero");
+        c[1][8] = new Torre("| t2 |", "nero");
+
+        for (int i = 1; i <= 8; i++) {
+            c[2][i] = new Pedone("| p" + i + " |", "nero");
+        }
+
+        for (int i = 3; i <= 7; i++) {
+            if (c[i][0] == null) c[i][0] = new CasellaVuota("" + i + "", null);
+            for (int j = 1; j <= 8; j++) {
+                if (c[i][j] == null) c[i][j] = new CasellaVuota("|    |", null);
+            }
+        }
+
+        c[7][0] = new CasellaVuota("2", null);
+        for (int i = 1; i <= 8; i++) {
+            c[7][i] = new Pedone("| p" + i + " |", "bianco");
+        }
+
+        c[8][0] = new CasellaVuota("1", null);
+        c[8][1] = new Torre("| t1 |", "bianco");
+        c[8][2] = new Cavallo("| c1 |", "bianco");
+        c[8][3] = new Alfiere("| a1 |", "bianco");
+        c[8][4] = new Regina("| q  |", "bianco");
+        c[8][5] = new Re("| re |", "bianco");
+        c[8][6] = new Alfiere("| a2 |", "bianco");
+        c[8][7] = new Cavallo("| c2 |", "bianco");
+        c[8][8] = new Torre("| t2 |", "bianco");
 
         return c;
     }
+
+
+
+
+
     public Mossa getPoszione(String nome, String colore){
         Mossa posAttuale = new Mossa();
         for (int i = 0; i < 8; i++) {
--- src/main/java/logic/ChessGame.java
+++ src/main/java/logic/ChessGame.java
@@ -4,12 +4,12 @@ import domain.Pezzo;
 import domain.Scacchiera;
 
 public class ChessGame {
-    static Pezzo[][] p = new Pezzo[8][8];
+    static Pezzo[][] p = new Pezzo[9][9];
     static Scacchiera c = new Scacchiera(p);
     public static void main(String[] args){
-    for(int i = 0; i<8; i++){
+    for(int i = 0; i<9; i++){
         System.out.println();
-        for(int j = 0; j<8; j++){
+        for(int j = 0; j<9; j++){
             System.out.print(c.p[i][j].getNome()+" ");
         }
     }
