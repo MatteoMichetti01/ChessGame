aa8aa2d56e2266b442ac7856f52a9de27453eba6
MatteoMichetti01 <michettimatteo827@gmail.com>
2024-03-06 16:23:18 +0100

    modifica di cassella e scacchiera

--- src/main/java/domain/Casella.java
+++ src/main/java/domain/Casella.java
@@ -1,20 +1,30 @@
 package domain;
 
 public class Casella {
+    String Posizione;
+
     String nome;
+
     Pezzo pezzo;
     public int posX,posY;
     public boolean occupata= false;
-
-    public Casella(String nome, Pezzo pezzo, int posX, int posY,boolean occupata){
-        this.nome=nome;
+    public String getPosizione(){
+        return Posizione;
+    }
+    public Casella(String Posizione, Pezzo pezzo, int posX, int posY, boolean occupata){
+        this.Posizione = Posizione;
         this.pezzo=pezzo;
         this.posX=posX;
         this.posY=posY;
         this.occupata=true;
 
     }
-    public Casella(String nome){this.nome=nome;}
+
+    public Casella(String nome, String Posizione){
+
+        this.Posizione = Posizione;
+        this.nome=nome;
+    }
 
     public String getNome() {
         if (occupata){return pezzo.getNome();}
@@ -23,6 +33,10 @@ public class Casella {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public void setPosizione(String posizione) {
+        this.Posizione = posizione;
     }
 
     public int getPosPezzoX() {
--- src/main/java/domain/Scacchiera.java
+++ src/main/java/domain/Scacchiera.java
@@ -12,57 +12,75 @@ public class Scacchiera {
 
     }
     public Casella[][] creazioneScacchiera(Casella[][] casella) {
-        casella[0][0] = new Casella("  ");
-        casella[0][1] = new Casella(" A   ");
-        casella[0][2] = new Casella("  B   ");
-        casella[0][3] = new Casella("  C   ");
-        casella[0][4] = new Casella("  D   ");
-        casella[0][5] = new Casella("  E   ");
-        casella[0][6] = new Casella("  F   ");
-        casella[0][7] = new Casella("  G   ");
-        casella[0][8] = new Casella("  H");
+        casella[0][0] = new Casella("  ","  ");
+        casella[0][1] = new Casella(" A   ", " ");
+        casella[0][2] = new Casella("  B   ", "");
+        casella[0][3] = new Casella("  C   ", "");
+        casella[0][4] = new Casella("  D   ", "");
+        casella[0][5] = new Casella("  E   ","");
+        casella[0][6] = new Casella("  F   ","");
+        casella[0][7] = new Casella("  G   ","");
+        casella[0][8] = new Casella("  H","");
 
         for (int i = 1; i <= 8; i++) {
-            casella[i][0] = new Casella("" + (9 - i) + "");
+            casella[i][0] = new Casella("" + (i) + "", "");
             for (int j = 1; j <= 8; j++) {
-                casella[i][j] = new Casella("|    |");
+                casella[i][j] = new Casella("|    |","");
             }
+
         }
 
-        casella[1][1] = new Casella("| t1 |", new Torre("|"+" t1 "+"|"),1,1,true);
-        casella[1][2] = new Casella("| c1 |", new Cavallo("|"+" c1 "+"|"),1,2,true);
-        casella[1][3] = new Casella("| a1 |", new Alfiere("|"+" a1 "+"|"),1,3,true);
-        casella[1][4] = new Casella("| q  |", new Regina("|"+" q  "+"|"),1,4,true);
-        casella[1][5] = new Casella("| re |", new Re("|"+" re "+"|"),1,5,true);
-        casella[1][6] = new Casella("| a2 |", new Alfiere("|"+" a2 "+"|"),1,6,true);
-        casella[1][7] = new Casella("| c2 |", new Cavallo("|"+" c2 "+"|"),1,7,true);
-        casella[1][8] = new Casella("| t2 |", new Torre("|"+" t2 "+"|"),1,8,true);
 
+        casella[1][1] = new Casella("A1", new Torre("|"+" t1 "+"|"),1,1,true);
+        casella[1][2] = new Casella("B1", new Cavallo("|"+" c1 "+"|"),1,2,true);
+        casella[1][3] = new Casella("C1", new Alfiere("|"+" a1 "+"|"),1,3,true);
+        casella[1][4] = new Casella("D1", new Regina("|"+" q  "+"|"),1,4,true);
+        casella[1][5] = new Casella("E1", new Re("|"+" re "+"|"),1,5,true);
+        casella[1][6] = new Casella("F1", new Alfiere("|"+" a2 "+"|"),1,6,true);
+        casella[1][7] = new Casella("G1", new Cavallo("|"+" c2 "+"|"),1,7,true);
+        casella[1][8] = new Casella("H1", new Torre("|"+" t2 "+"|"),1,8,true);
+
+        String[]  lettere = new String[]{"j", "A", "B", "C", "D", "E", "F", "G", "H"};
         for (int i = 1; i <= 8; i++) {
-            casella[2][i] = new Casella("A"+(8-i),new Pedone("| p" + i + " |"),2,i,true);
+            casella[2][i] = new Casella(lettere[i]+"2",new Pedone("| p" + i + " |"),2,i,true);
         }
 
         for (int i = 3; i <= 7; i++) {
-            casella[i][0] = new Casella("" + i + "");
+            casella[i][0] = new Casella("" + (i) + "","");
             for (int j = 1; j <= 8; j++) {
-                casella[i][j] = new Casella("|    |");
+                casella[i][j] = new Casella("|    |",lettere[j]+i);
             }
         }
 
-        casella[7][0] = new Casella("2");
+        /*for (int i = 1; i <= 8; i++) {
+            casella[2][i] = new Casella(lettere[i]+"2",new Pedone("| p" + i + " |"),2,i,true);
+        }*/
+
+        casella[7][0] = new Casella("7","");
         for (int i = 1; i <= 8; i++) {
-            casella[7][i] = new Casella(" ",new Pedone("| p" + i + " |"),7,i,true);
+            casella[7][i] = new Casella(lettere[i]+"7",new Pedone("| p" + i + " |"),7,i,true);
         }
 
-        casella[8][0] = new Casella("1");
-        casella[8][1] = new Casella(" ", new Torre("| t1 |"),8,1,true);
-        casella[8][2] = new Casella(" ",new Cavallo("| c1 |"),8,2,true);
-        casella[8][3] = new Casella (" ",new Alfiere("| a1 |"),8,3,true);
-        casella[8][4] = new Casella(" ",new Regina("| q  |"),8,4,true);
-        casella[8][5] = new Casella(" ",new Re("| re |"),8,5,true);
-        casella[8][6] = new Casella(" ",new Alfiere("| a2 |"),8,6,true);
-        casella[8][7] = new Casella (" ", new Cavallo("| c2 |"),8,7,true);
-        casella[8][8] = new Casella (" ",new Torre("| t2 |"),8,8,true);
+        casella[8][0] = new Casella("8","");
+        casella[8][1] = new Casella("A8", new Torre("| t1 |"),8,1,true);
+        casella[8][2] = new Casella("B8",new Cavallo("| c1 |"),8,2,true);
+        casella[8][3] = new Casella ("C8",new Alfiere("| a1 |"),8,3,true);
+        casella[8][4] = new Casella("D8",new Regina("| q  |"),8,4,true);
+        casella[8][5] = new Casella("E8",new Re("| re |"),8,5,true);
+        casella[8][6] = new Casella("F8",new Alfiere("| a2 |"),8,6,true);
+        casella[8][7] = new Casella ("G8", new Cavallo("| c2 |"),8,7,true);
+        casella[8][8] = new Casella ("H8",new Torre("| t2 |"),8,8,true);
+
+
+
+
+        //inserimento nome per attributo di casella con coordinate giuste di scachiera
+
+
+       // for(int i=2; i<8; i++) {
+         //   for (int j= 0; j < 8; j++) casella[i][j].setNome(lettere[i] + j);
+       // }
+
 
         return casella;
     }
