99c753861614f17c4359a7361c5cd3887ff0706d
MatteoMichetti01 <michettimatteo827@gmail.com>
2024-03-09 11:42:27 +0100

    aggiunta la classe scacco con metodo che calcola le possibili caselle dove il re puo posizionarsi, il metodo per il controllo dello scacco è da revisionare

---
+++ src/main/java/controller/Scacco.java
@@ -0,0 +1,67 @@
+package controller;
+
+import domain.Casella;
+import domain.Scacchiera;
+import logic.Giocatore;
+import logic.MossaNonValida;
+
+import java.util.*;
+public class Scacco {
+
+    public ArrayList<Casella> casellePossibiliReNero(Scacchiera scacchiera){
+        ArrayList<Casella> caselleReNero = new ArrayList<>();
+        for(int i = 0 ; i<9; i ++){
+            for(int j = 0 ; j< 9 ; j++){
+                if(scacchiera.casella[i][j].getPezzo().getNome().equals("re") && scacchiera.casella[i][j].getPezzo().getColore().equals("nero")){
+                    //dx
+                    caselleReNero.add(scacchiera.casella[i+1][j-1]);
+                    caselleReNero.add(scacchiera.casella[i+1][j]);
+                    caselleReNero.add(scacchiera.casella[i+1][j+1]);
+
+                    caselleReNero.add(scacchiera.casella[i][j-1]);
+                    caselleReNero.add(scacchiera.casella[i][j+1]);
+                    //sx
+                    caselleReNero.add(scacchiera.casella[i-1][j-1]);
+                    caselleReNero.add(scacchiera.casella[i-1][j]);
+                    caselleReNero.add(scacchiera.casella[i-1][j+1]);
+
+
+                }
+            }
+        }
+        //avevo fatto un for each sostituito poi con questo removeIf
+        caselleReNero.removeIf(Casella::isOccupata);
+        return caselleReNero;
+    }
+
+    public String PosReNero(Scacchiera scacchiera) {
+        String posReN="";
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (scacchiera.casella[i][j].getPezzo().getNome().equals("re") && scacchiera.casella[i][j].getPezzo().getColore().equals("nero")) {
+                    posReN = scacchiera.casella[i][j].getPosizione();
+                }
+
+            }
+        }
+        return posReN;
+
+    }
+
+
+    //questo metodo esegue una mossa con ogni pezzo bianco che lo va a posizionare sulla casella del re nero
+    //in teoria se non viene sollevata l'eccezione "pezzi in mezzo" vuol dire che il re è sotto scacco da parte di qualche pezzo
+    public boolean controlloScaccoReNero(Scacchiera scacchiera) throws MossaNonValida {
+        for(int i = 0 ; i<9; i ++){
+            for(int j = 0 ; j< 9 ; j++){
+                if(scacchiera.casella[i][j].getPezzo().getColore().equals("bianco")){
+                    PezzoService p1 = new PezzoService(scacchiera);
+                    String PosReNero= PosReNero(scacchiera);
+                    p1.move(scacchiera.casella[i][j].getPezzo().getNome(), PosReNero,scacchiera.casella[i][j].getPezzo().getColore());
+                }
+
+                }
+
+
+    }
+}
--- src/main/java/domain/Re.java
+++ src/main/java/domain/Re.java
@@ -9,4 +9,5 @@ public class Re extends Pezzo {
     public Re(String nome, String colore) {
         super(nome, colore);
     }
+
 }
